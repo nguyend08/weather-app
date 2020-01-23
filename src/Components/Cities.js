ace605191164b2af7137bd079cbd665b8709f49e
Daniel <dahnguyen@ucdavis.edu>
2020-01-23 13:59:21 -0800

    added conditional rendering to search for city when click add city button.  Need to style.  Added fix for CORS issue, able to render weather API

--- src/Components/Cities.js
+++ src/Components/Cities.js
@@ -1,19 +1,27 @@
 import React, {Component} from 'react'
 import '../Styles/Cities-Styles.scss'
+import WEATHER_API_KEY from '../weather-key'
 
 class Cities extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      cities: []
+      cities: [],
+      transparent: false,
+      currentCity: '',
     }
+    this.addCity = this.addCity.bind(this);
+    this.changeTransparent = this.changeTransparent.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleInputSubmit = this.handleInputSubmit.bind(this);
   }
 
   addCity(city) {
-    fetch(`api.openweathermap.org/data/2.5/weather?q=${city}`)
+    fetch(`https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?q=${city}&appid=${WEATHER_API_KEY}`)
       .then(results => results.json())
       .then(data => {
+        console.log(data)
         this.setState(prevState =>({
           cities: [...prevState.cities, data]
         }))
@@ -21,14 +29,56 @@ class Cities extends Component {
       .catch(error => console.error('Error:', error))
   } 
 
+  changeTransparent() {
+    this.setState({
+      transparent: !this.state.transparent
+    })
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      currentCity: e.target.value
+    })
+  }
+
+  handleInputSubmit(e) {
+    e.preventDefault();
+    this.addCity(this.state.currentCity)
+  }
+
   render() {
-    return(
+    return (
       <div className='cities-container'>
-        <div className='add-container'>
+        {
+        this.state.transparent 
+        ? 
+        <div className='transparent-container'>
+          <form 
+            className='transparent-input'
+            onSubmit={this.handleInputSubmit}
+          >
+            <label>
+              Please Insert A City:
+              <input 
+                type='text' 
+                value={this.state.currentCity}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <input type='submit' value='Submit'/>
+          </form>
+        </div>
+        :
+        <div 
+          className='add-container'
+          onClick={this.changeTransparent}
+        >
           <h4 className='add-header'>ADD CITY</h4>
           <div className='add-button'></div>
-          <img className='add-button-picture' src='http://www.freepngclipart.com/download/cartoon/78468-information-city-vinnytsia-cartoon-stryzhavka-download-hd-png.png'></img>
+          <img className='add-button-picture' alt='city buildings' src='http://www.freepngclipart.com/download/cartoon/78468-information-city-vinnytsia-cartoon-stryzhavka-download-hd-png.png'></img>
         </div>
+        }
+        
       </div>
     )
   }
---
+++ src/weather-key.js
@@ -0,0 +1,3 @@
+const WEATHER_API_KEY = 'a9bd3d4534b05f1d5695b152c00e7c34'
+
+export default WEATHER_API_KEY
