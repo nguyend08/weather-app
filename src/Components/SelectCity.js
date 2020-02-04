cfc1e50fbb05052743a5161e41d23063a1519b41
Daniel <dahnguyen@ucdavis.edu>
2020-02-04 14:34:17 -0800

    added mixin to DRY displayed-cities.  Fixed position of selectcity

--- src/Components/SelectCity.js
+++ src/Components/SelectCity.js
@@ -1,20 +1,21 @@
 import React from "react";
+import '../Styles/SelectCity-Styles.scss';
 
 function SelectCity(props) {
   return (
     <div className="select-city-container">
-      <form className="select-city-input" onSubmit={props.handleInputSubmit}>
+      <div className="select-city-exit" onClick={props.changeTransparent}>X</div>
+      <form className="select-city-form" onSubmit={props.handleInputSubmit}>
         <label>
-          Please Insert A City:
+          Select A City:
+        </label>
           <input
             type="text"
             value={props.currentCity}
             onChange={props.handleInputChange}
           />
-        </label>
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Add City" />
       </form>
-      <div onClick={props.changeTransparent}>X</div>
     </div>
   );
 }
